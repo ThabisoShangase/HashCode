580b8914a9f131967722c81a4df5e3ee7d183ca2
NkanyisoT <tn.thwala@gmail.com>
2019-02-28 22:17:58 +0200

    something

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,9 @@ public class Main {
     static int verticals = 0;
 
     public static void main(String[] args) throws Exception {
+//        FileReader fileReader = new FileReader("C:\\do\\hash\\HashCode\\src\\main\\resources\\c_memorable_moments.txt");
         FileReader fileReader = new FileReader("C:\\do\\hash\\HashCode\\src\\main\\resources\\b_lovely_landscapes.txt");
+//        FileReader fileReader = new FileReader("C:\\do\\hash\\HashCode\\src\\main\\resources\\d_pet_pictures.txt");
 
         Scanner fileScanner = new Scanner(fileReader);
 
@@ -44,7 +46,8 @@ public class Main {
         System.out.println(photos.size());
 
         FileWriter fileWriter = new FileWriter("C:\\do\\hash\\HashCode\\src\\main\\resources\\output.txt");
-        dealWithThemPhotos(photos, fileWriter);
+//        dealWithThemPhotos(photos, fileWriter);
+        dealWithVerticalPhotos(photos, fileWriter);
         fileWriter.flush();
         fileWriter.close();
     }
@@ -58,4 +61,116 @@ public class Main {
         }
     }
 
+    public static void dealWithVerticalPhotos(List<Photo> photos, FileWriter fileWriter) throws IOException {
+        int numberOfFrames = 0;
+        String end = "";
+        Frame frame = new Frame();
+
+        for (Photo photo: photos) {
+            if (photo.getAlignment() == Alignment.VERTICAL && !frame.isComplete()) {
+                frame.addPhoto(photo);
+            } else if(photo.getAlignment() == Alignment.HORIZONTAL){
+                end = end + photo.getId() + "\n";
+                numberOfFrames++;
+            }
+
+            if (frame.isComplete()){
+                end = end + frame.getPhotos().get(0).getId() + " " + frame.getPhotos().get(1).getId() + "\n";
+                frame = new Frame();
+                numberOfFrames++;
+            }
+        }
+        fileWriter.append(numberOfFrames + "\n");
+        fileWriter.append(end);
+    }
+
+
+
 }
+
+enum Alignment {
+
+    VERTICAL("V"),
+    HORIZONTAL("H");
+
+    String description;
+
+    Alignment(String description) {
+        this.description = description;
+    }
+
+    public static Alignment findByDescription(String description) {
+        for (Alignment alignment: values()) {
+            if (alignment.description.equals(description)) {
+                return alignment;
+            }
+        }
+        return null;
+    }
+}
+
+class Photo {
+    int id;
+    Alignment alignment;
+    int numOfTags;
+    List<String> tags = new ArrayList<>();
+
+    public Photo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public Alignment getAlignment() {
+        return alignment;
+    }
+
+    public void setAlignment(final Alignment alignment) {
+        this.alignment = alignment;
+    }
+
+    public int getNumOfTags() {
+        return numOfTags;
+    }
+
+    public void setNumOfTags(final int numOfTags) {
+        this.numOfTags = numOfTags;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(final List<String> tags) {
+        this.tags = tags;
+    }
+}
+
+class Frame{
+    List<Photo> photos = new ArrayList<>();
+
+    public Frame() {
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+
+    public void addPhoto(Photo photo){
+        photos.add(photo);
+    }
+
+    public boolean isComplete(){
+        return photos.size() == 2;
+    }
+}
+
